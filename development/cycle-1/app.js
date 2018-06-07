e9cb79d377177b821de866346a9d27d53e6cd7d3
Ashish Singh <ashishcodes4@gmail.com>
2018-06-07 08:26:13 +0530

    cycle-1

---
+++ development/cycle-1/app.js
@@ -0,0 +1,7 @@
+var numSquares = 6;
+var colors = [];
+var pickedColor;
+var squares = document.querySelectorAll(".square");
+var messageDisplay = document.querySelector("#message");
+var resetButton = document.querySelector("#reset");
+var modeButtons = document.querySelectorAll(".mode");
