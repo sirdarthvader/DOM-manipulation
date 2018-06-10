2c335a139f2c30b20580631c29c4db6af1389194
Ashish Singh <ashishcodes4@gmail.com>
2018-06-10 06:10:46 +0530

    cycle-2

--- development/cycle-1/app.js
+++ development/cycle-1/app.js
@@ -5,7 +5,14 @@ var messageDisplay = document.querySelector("#message");
 var resetButton = document.querySelector("#reset");
 var modeButtons = document.querySelectorAll(".mode");
 
-var colors = pickColor();
+var colors = [
+    "rgb(255, 0, 0)",
+    "rgb(0, 255, 0)", 
+    "rgb(0, 0, 255)", 
+    "rgb(255, 255, 0)", 
+    "rgb(255, 0, 255)",
+    "rgb(0, 255, 255)"
+];
 
 pickedColor = colors[3];
 
